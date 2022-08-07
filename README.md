d3a2d4185b5e5b894c28590f1b28663d2846ddef
Parthipan Natkunam <natkunamparthipan@gmail.com>
2022-08-07 22:29:51 +0200

    finish up demo

---
+++ README.md
@@ -0,0 +1,5 @@
+## SSE Demo APP with Node.js
+
+A demo application of a simple node server that streams events using SSE technique and a simple js client that subscribes and consumes the stream through `EventSource` API.
+
+Built for a tech article.
--- server/index.js
+++ server/index.js
@@ -1,23 +1,38 @@
 const http = require("http");
+var os = require("os");
 
 const host = "127.0.0.1";
 const port = 8080;
 
+
+// A simple dataSource that changes over time
+let dataSource = 0;
+const updateDataSource = () => {
+  const delta = Math.random();
+  dataSource += delta;
+}
+
+
 const requestListener = function (req, res) {
-  switch (req.url) {
-    case "/stream":
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end('<h1>Stream route</h1>')
-      break;
-    default:
-      res.statusCode = 404;
-      res.end("resource does not exist");
-      break;
+  if (req.url === '/ticker') {
+    res.statusCode = 200;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("connection", "keep-alive");
+    res.setHeader("Content-Type", "text/event-stream");
+
+    setInterval(() => {
+      const data = JSON.stringify({ ticker: dataSource });
+      res.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${data}\n\n`);
+    }, 1000);
+  } else {
+    res.statusCode = 404;
+    res.end("resource does not exist");
   }
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
+  setInterval(()=> updateDataSource(), 500);
   console.log(`server running at http://${host}:${port}`);
 });
